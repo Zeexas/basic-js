4f2bbdb2e8f5fa9acd0db05f48bfb7959fea27c7
zeexas <email_id>
2021-09-30 00:00:58 +0500

    fix: solve tasks cats, dream-team, sum-digits

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let names = members.filter(function(n) {
+    return typeof n === 'string';
+  });
+  if (names.length === 0) {
+    return false;
+  } else {
+    return names.map(n => n.toUpperCase().trim()[0]).sort().join('');
+  }
+  
+  // return members.filter(n => typeof n === 'string').map(x => x[0]).sort().join('');
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  const checkDate = new Date(date);
+  const month = checkDate.getMonth();
+  
+  if (date === '') {
+    return 'Unable to determine the time of year!';
+  } else if (!(checkDate instanceof Date)) {
+    return 'Invalid date!'
+  } else {
+    if (month === 0 || month === 1 || month === 11) {
+      return 'winter';
+    } else if (month > 1 && month < 4) {
+      return 'spring';
+    } else if (month > 4 && month < 7) {
+      return 'summer';
+    } else {
+      return 'autumn (fall)';
+    }
+  }
 }
