4971fb348a6125aee2d2ed96e849530084e0d2a3
zeexas <email_id>
2021-09-30 20:01:21 +0500

    fix: st-encode-line solution

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let cleanS1 = (Array.from(new Set(s1.split(''))));
+  for (let i = 0; i < cleanS1.length; i++) {
+    if (s1.split('').filter(x => x === cleanS1[i]).length <= s2.split('').filter(x => x === cleanS1[i]).length) {
+      count += s1.split('').filter(x => x === cleanS1[i]).length;
+    } else {
+      count += s2.split('').filter(x => x === cleanS1[i]).length;
+    }
+  }
+  return count;
 }
